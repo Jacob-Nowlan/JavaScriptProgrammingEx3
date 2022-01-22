1fe3b52e67353428ea645634fd590c788e1bde3e
Jacob-Nowlan <94065183+Jacob-Nowlan@users.noreply.github.com>
2022-01-22 00:00:52 -0400

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,18 @@
+
+let now = new Date();
+console.log(now);
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(`${now.toDateString()} ${now.toTimeString()}`);
+console.log(`Now Year: ${now.getFullYear()}`);
+console.log(`Now Month: ${now.getMonth()}`); // zero based
+console.log(`Now Date: ${now.getDate()}`);
+console.log(`Now Day of Week: ${now.getDay()}`); // zero based. Sunday is DoW zero
+console.log(`Now Hours: ${now.getHours()}`);
+console.log(`Now Minutes: ${now.getMinutes()}`);
+console.log(`Now Seconds: ${now.getSeconds()}`);
+console.log(`Now Milliseconds: ${now.getMilliseconds()}`);
+
+now.setFullYear(1999);
+console.log(`${now.toDateString()} ${now.toTimeString()}`);
+console.log(`Then Day of Week: ${now.getDay()}`);
